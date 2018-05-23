c2a7dbdd31e133c04d770742d177971657bf746e
Charles Hill <chill@degreesofzero.com>
2018-05-23 21:34:41 +0200

    Github pages (homepage)

--- js/handlebars.extend/i18n.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-	'use strict';
-
-	Handlebars.registerHelper('i18n', function() {
-		return app.i18n.t.apply(app.i18n, arguments);
-	});
-
-})();
--- js/handlebars.extend/numbers.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-	'use strict';
-
-	Handlebars.registerHelper('formatNumber', function(number, format) {
-		return app.util.formatNumber(number, format);
-	});
-
-})();
